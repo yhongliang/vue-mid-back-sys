9560002418c82e6a64ace0df5407ce9cb217cc0e
yhongliang <1126980097@qq.com>
2025-05-09 17:00:54 +0800

    fix:添加hamburger

--- src/layout/components/Navbar/index.vue
+++ src/layout/components/Navbar/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <div class="navbar">
+            <hamburger class="hamburger-container" />
             <div class="right-menu">
                 <!-- 用户信息 -->
                 <el-dropdown>
@@ -22,6 +23,7 @@
 <script setup lang="ts">
 import { useUserStore } from '@/stores/user'
 import { useRouter } from 'vue-router'
+import Hamburger from '@/layout/components/Hamburger/index.vue'
 const router = useRouter()
 
 const userStore = useUserStore()
@@ -31,4 +33,24 @@ const logout = () => {
 }
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.navbar {
+    height: 50px;
+    overflow: hidden;
+    position: relative;
+    box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+
+    .hamburger-container {
+        line-height: 46px;
+        height: 100%;
+        float: left;
+        cursor: pointer;
+        transition: background 0.3s;
+        -webkit-tap-highlight-color: transparent;
+
+        &:hover {
+            background: rgba(0, 0, 0, 0.025);
+        }
+    }
+}
+</style>
